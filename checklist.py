ee9a99ea587e0fdf2bbd112fdbc1985e4363b40a
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2019-09-03 12:52:37 -0700

    I Created fuction and READ

--- checklist.py
+++ checklist.py
@@ -1,4 +1,19 @@
 # hello world
 print("Hello World")
-
+checklist = list()
 # I will work on this project later
+# checklist = list()
+checklist.append('Blue')
+print(checklist)
+checklist.append('Orange')
+print(checklist)
+
+#create function 
+def create(item):
+    checklist.append(item)
+#READ function
+def read(index):
+    return checklist[index]
+
+
+    
